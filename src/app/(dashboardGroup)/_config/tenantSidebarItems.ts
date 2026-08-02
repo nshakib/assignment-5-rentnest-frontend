1c105a912206e3b186ba9e39a32a846c7af6ff36
nshakib <nazmus.shakib.cse@gmail.com>
2026-08-02 22:23:18 +0600

    feat: improve sidebar nav icon

--- src/app/(dashboardGroup)/_config/tenantSidebarItems.ts
+++ src/app/(dashboardGroup)/_config/tenantSidebarItems.ts
@@ -1,30 +1,30 @@
 import { ISidebarItem } from "@/lib/types";
-import { FileText, LayoutDashboard } from "lucide-react";
+import { ClipboardList, CreditCard, FileText, LayoutDashboard, Star, User } from "lucide-react";
 
-export const TENANT_SIDEBAR_ITEMS : ISidebarItem[] = [
-    {
-        label : "Tenant Dashboard",
-        href : "/tenant-dashboard",
-        icon : LayoutDashboard
-    },
-    {
-        label : "My Requests",
-        href : "/tenant-dashboard/requests",
-        icon : FileText
-    },
-    {
-        label : "Payments",
-        href : "/tenant-dashboard/payments",
-        icon : FileText
-    },
-    {
-        label : "My Reviews",
-        href : "/tenant-dashboard/reviews",
-        icon : FileText
-    },
-    {
-        label : "Profile",
-        href : "/tenant-dashboard/profile",
-        icon : FileText
-    },
-]
+export const TENANT_SIDEBAR_ITEMS: ISidebarItem[] = [
+  {
+    label: "Dashboard",
+    href: "/tenant-dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "My Requests",
+    href: "/tenant-dashboard/requests",
+    icon: ClipboardList,
+  },
+  {
+    label: "My Payments",
+    href: "/tenant-dashboard/payments",
+    icon: CreditCard,
+  },
+  {
+    label: "My Reviews",
+    href: "/tenant-dashboard/reviews",
+    icon: Star,
+  },
+  {
+    label: "Profile",
+    href: "/tenant-dashboard/profile",
+    icon: User,
+  },
+];
